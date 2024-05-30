afa7364c91e9c3978c5cdf7726fb95da85d71fbb
lucas50000 <61808310+lucas50000@users.noreply.github.com>
2024-05-30 09:31:35 -0300

    1.2.25

--- src/App.js
+++ src/App.js
@@ -6,32 +6,19 @@ import Home from './pages/HomePag';
 import "./Styles/Components/Pages/HomePage.css";
 import ContactoPage from './pages/ContactoPage';
 import './Styles/Components/Pages/ContactoPage.css';
-export let pag = 0;
-let PaginaActual=ContactoPage;
-
-
-function Cpag(){
-  switch(pag){
-    case 0:
-      PaginaActual = ContactoPage;
-    break
-    case 1:
-      PaginaActual = Home;
-    break
-    default:
-      
-    break
-  }
-}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App(cont) {
   return (
     <div className="App">
-      <Nav />
-      <PaginaActual />
-      <PaginaActual />
-      <PaginaActual />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Contacto' element={<ContactoPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
