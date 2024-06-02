fdd59e3c7fb7f88168996f52e138ddd85cc5499b
juanMarcosChalu <chalujuanmarcos@gmail.com>
2024-06-02 20:41:41 -0300

    actualización pagina tienda

--- src/pages/TiendaPage.js
+++ src/pages/TiendaPage.js
@@ -2,59 +2,80 @@ import React from 'react'
 
 const TiendaPage = (props) => {
   return (
+    <div>
+      <head><style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+</style></head>
     <div id='divTienda'>
         <ol id='gridTienda'>
-          <li className='producto'>
-            <div className="productoImagen">
+        <li className='producto'>
               <a href='3' className='productolink'>
-              <div className='productoContenedor'>
-              <img src='./imagenes/casa.png'></img>
-              
-              </div>
+                <div className='productoContenedor'>
+                  <img src='https://http2.mlstatic.com/D_Q_NP_991845-MLA74734455351_022024-W.webp' className='productoImagen'></img>
+                  <div className='productoTexto'>
+                    <p className='productoDescripcion'>sata ssd 480gb kingston</p>
+                    <h2 className='productoPrecio'>$ 20.000</h2>
+                  </div>
+                </div>
               </a>
-            </div>
           </li>
           <li className='producto'>
-            <div className="productoImagen">
               <a href='3' className='productolink'>
-              <img src='./imagenes/casa.png'></img>
-              <p>20.000</p>
+                <div className='productoContenedor'>
+                  <img src='https://http2.mlstatic.com/D_Q_NP_991845-MLA74734455351_022024-W.webp' className='productoImagen'></img>
+                  <div className='productoTexto'>
+                    <p className='productoDescripcion'>sata ssd 480gb kingston</p>
+                    <h2 className='productoPrecio'>$ 20.000</h2>
+                  </div>
+                </div>
               </a>
-            </div>
           </li>
           <li className='producto'>
-            <div className="productoImagen">
               <a href='3' className='productolink'>
-              <img src='./imagenes/casa.png'></img>
-              <p>20.000</p>
+                <div className='productoContenedor'>
+                  <img src='https://http2.mlstatic.com/D_Q_NP_991845-MLA74734455351_022024-W.webp' className='productoImagen'></img>
+                  <div className='productoTexto'>
+                    <p className='productoDescripcion'>sata ssd 480gb kingston</p>
+                    <h2 className='productoPrecio'>$ 20.000</h2>
+                  </div>
+                </div>
               </a>
-            </div>
           </li>
           <li className='producto'>
-            <div className="productoImagen">
               <a href='3' className='productolink'>
-              <img src='./imagenes/casa.png'></img>
-              <p>20.000</p>
+                <div className='productoContenedor'>
+                  <img src='https://http2.mlstatic.com/D_Q_NP_991845-MLA74734455351_022024-W.webp' className='productoImagen'></img>
+                  <div className='productoTexto'>
+                    <p className='productoDescripcion'>sata ssd 480gb kingston</p>
+                    <h2 className='productoPrecio'>$ 20.000</h2>
+                  </div>
+                </div>
               </a>
-            </div>
           </li>
           <li className='producto'>
-            <div className="productoImagen">
               <a href='3' className='productolink'>
-              <img src='./imagenes/casa.png'></img>
-              <p>20.000</p>
+                <div className='productoContenedor'>
+                  <img src='https://http2.mlstatic.com/D_Q_NP_991845-MLA74734455351_022024-W.webp' className='productoImagen'></img>
+                  <div className='productoTexto'>
+                    <p className='productoDescripcion'>sata ssd 480gb kingston</p>
+                    <h2 className='productoPrecio'>$ 20.000</h2>
+                  </div>
+                </div>
               </a>
-            </div>
           </li>
           <li className='producto'>
-            <div className="productoImagen">
               <a href='3' className='productolink'>
-              <img src='./imagenes/casa.png'></img>
-              <p>20.000</p>
+                <div className='productoContenedor'>
+                  <img src='https://http2.mlstatic.com/D_Q_NP_991845-MLA74734455351_022024-W.webp' className='productoImagen'></img>
+                  <div className='productoTexto'>
+                    <p className='productoDescripcion'>sata ssd 480gb kingston</p>
+                    <h2 className='productoPrecio'>$ 20.000</h2>
+                  </div>
+                </div>
               </a>
-            </div>
           </li>
         </ol>
+    </div>
     </div>
   );
 }
