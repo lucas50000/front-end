1168a7ef6a7802ffb8ed17ebf9d357b9ee9fdf4b
juanMarcosChalu <chalujuanmarcos@gmail.com>
2024-06-06 14:42:53 -0300

    fondoparalasimagenesdetienda´pag

--- src/pages/TiendaPage.js
+++ src/pages/TiendaPage.js
@@ -6,10 +6,7 @@ const TiendaPage = (props) => {
       <head><style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 </style></head>
-<div class="producto2">
-    <a href="#">Enlace del Producto</a>
-    <div class="hover-effect"></div>
-</div>
+
 
     <div id='divTienda'>
         <ol id='gridTienda'>
