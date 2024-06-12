9a02f6cbb41ebb5e90145aaa532e7f8144afaf10
k38bru <145170242+k38bru@users.noreply.github.com>
2024-06-12 09:35:40 -0300

    1.1.0
    a

--- src/pages/HomePag.js
+++ src/pages/HomePag.js
@@ -11,7 +11,7 @@ const Home = (props) => {
           Descubre los mejores precios de alimentos, de higiene y uso personal
           en Benitos Juarez. 
         </p>
-        <a href="https://imgs.search.brave.com/BhdoNSyYyQP-Uc-lO0DFpc7PmwO6U1SqM_NPaJ2a0JA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXM3Lm1lbWVkcm9p/ZC5jb20vaW1hZ2Vz/L1VQTE9BREVENTc1/LzY2MTE0MmY1NGNj/ODcuanBlZw" target="_blank" id="A">
+        <a href="#" target="_blank" id="A">
           Explorar
         </a>
       </div>
