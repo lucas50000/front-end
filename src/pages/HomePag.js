fc4a5b9fca044ead615af007c6ca839a42e8e422
k38bru <145170242+k38bru@users.noreply.github.com>
2024-10-04 11:39:21 -0300

    pequeños cambios 2
    la secuela

--- src/pages/HomePag.js
+++ src/pages/HomePag.js
@@ -1,29 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 const Home = (props) => {
-  
-
   return (
     <div id="bienvenidos">
-      <h1 id="titulo">Bienvenidos</h1>
+      <h1 id="titulo">BIENVENIDOS</h1>
       <div id="texto">
         <p>
-          
-          Descubre los mejores precios de alimentos, de higiene y uso personal
-          en Benitos Juarez. <br/>Comparte las mejores ofertas que encuentres para cooperar con nuestra comunidad.
+          En esta página, podrás comparar los precios de una amplia variedad de
+          productos de diferentes categorías, desde tecnología y moda hasta
+          artículos para el hogar y más.
+          <br /> Nuestro objetivo es ofrecerte una experiencia fácil y
+          agradable, Aquí TÚ TIENES EL CONTROL. Esperamos que disfrutes y que
+          encuentres exactamente lo que buscas.
         </p>
         <Link to="/Tienda" className="no-underline">
           <button target="_blank" id="botoninicio">
-          Explorar
+            Explorar
           </button>
         </Link>
       </div>
       <div id="imagenes">
-        <img src="./imagenes/carrito.jpg" alt="" />
-        <img src="./Imagenes/fotohome2.jpg" alt=""  />
+        <img src="./imagenes/carrito.jpg" alt="producto" />
+        <img src="./Imagenes/fotohome2.jpg" alt="productos" />
       </div>
     </div>
   );
 };
 export default Home;
-
